8c73535e0b64ced9be04013765379bff78c28a93
Jonathan Neyra <neyranievesjonathan@gmail.com>
2025-01-21 10:50:46 -0600

    feat: ssm handler wit params

--- weather_api/src/app.py
+++ weather_api/src/app.py
@@ -7,8 +7,23 @@ from send_forecast.domain.service_entities import Recipient, Location
 from send_forecast.infrastructure.sms_twilio_messanger import SMSTwilioMessanger
 from send_forecast.services.forecast_twlio_sender import ForecastTwilioSender
 from send_forecast.services.generate_forecast_message_from import GenerateForecastMessageFrom
+from utils.ssm_handler import assign_project_env_vars
 
 load_dotenv()
+
+if os.environ['TWILIO_ACCOUNT_SID'] is None:
+    param_lists = {
+        'to_decrypt': [
+            ('/ec2/deployment/credentials/twilio-account-sid-param', 'TWILIO_ACCOUNT_SID'),
+            ('/ec2/deployment/credentials/twilio-auth-token-param', 'TWILIO_AUTH_TOKEN'),
+            ('/ec2/deployment/values/twilio-phone-number-param', 'TWILIO_PHONE_NUMBER'),
+            ('/ec2/deployment/credentials/weather-api-key-param', 'API_KEY_WAPI'),
+        ],
+        'plain': [
+            ('/ec2/deployment/values/to-phone-number-param', 'TO_PHONE_NUMBER')
+        ]
+    }
+    assign_project_env_vars(param_lists)
 
 if __name__ == "__main__":
     location = Location("Noruega")
---
+++ weather_api/src/utils/ssm_handler.py
@@ -0,0 +1,40 @@
+import os
+import boto3
+
+
+class SsmHandler:
+    def __init__(self, param_lists, region_name=None):
+        self.smm_client = boto3.client('ssm', region_name=region_name or 'east-us-1')
+        self.param_lists = param_lists
+
+    def perform(self):
+        result = []
+        for param, env_name in self.param_lists['to_decrypt']:
+            value = self.decrypt_ssm_patameter(param)
+            result.append((env_name, value))
+
+        for param, env_name in self.param_lists['plain']:
+            value = self.plain_ssm_parameter(param)
+            result.append((env_name, value))
+
+        return result
+
+    def decrypt_ssm_patameter(self, parameter_name):
+        response = self.smm_client.get_parameter(
+            Name=parameter_name,
+            WithDecryption=True
+        )
+
+        return response['Parameter']['Value']
+
+    def plain_ssm_parameter(self, parameter_name):
+        response = self.smm_client.get_parameter(Name=parameter_name)
+
+        return response['Parameter']['Value']
+
+
+def assign_project_env_vars(param_lists):
+    ssm_vars = SsmHandler(param_lists)
+    envs = ssm_vars.perform()
+    for env_name, env_value in envs:
+        os.environ[env_name] = env_value
